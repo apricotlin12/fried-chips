b1d42aedc1415e581092aa97a6e4f31d001b7a6f
apricotlin12 <apricotlin@gmail.com>
2025-09-10 23:49:47 +0800

    20250910 - 整理分類檔案

---
+++ js/import-html.js
@@ -0,0 +1,33 @@
+// 讀取 header.html
+fetch('header.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+
+  // 修改 h1
+  const pageTitle = "活動倒數計時器";
+  if (typeof pageTitle !== 'undefined') {
+    const h1 = document.querySelector('#header-placeholder h1');
+    if (h1) h1.textContent = pageTitle;
+  }
+
+  // 設定 active class
+  const currentPath = window.location.pathname.split('/').pop(); // 例如 index.html
+  const links = document.querySelectorAll('#header-placeholder nav a');
+  links.forEach(link => {
+    const linkPath = link.getAttribute('href');
+    if (linkPath === currentPath) {
+      link.classList.add('active');
+    }
+    if (currentPath === "" && linkPath === "index.html") {
+      link.classList.add('active');
+    }
+  });
+});
+
+// 讀取 footer.html
+fetch('footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
